7160facd6b592e8e398e2395faf752ac5f8856c9
chaylak9 <50636411+chaylak9@users.noreply.github.com>
2019-05-16 10:58:03 -0700

    Add files via upload

---
+++ CSS/script.js
@@ -0,0 +1,10 @@
+<script>
+function myFunction() {
+var x = document.getElementById("responsiveNav");
+if (x.className === "mainNav") {
+x.className += " responsive";
+} else {
+x.className = "mainNav";
+}
+}
+</script>
